9049e2e5450ea4fc7468a088140b1f553ef00725
Tim Schlechter <timschlechter@gmail.com>
2015-03-04 21:57:24 +0100

    Added demo projects for the basic annotations approach, and one which uses JsonNET

---
+++ samples/Nancy.Swagger.Annotations.Demo/Models/Pet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Swagger.Annotations.Attributes;
+
+namespace Nancy.Swagger.Annotations.Demo.Models
+{
+    public class Pet
+    {
+        public long Id { get; set; }
+        public Category Category { get; set; }
+
+        [SwaggerModelProperty(Required = true)]
+        public string Name { get; set; }
+
+        [SwaggerModelProperty(Required = true)]
+        public List<string> PhotoUrls { get; set; }
+
+        public List<Tag> Tags { get; set; }
+
+        [SwaggerModelProperty(Description = "pet status in the store")]
+        [SwaggerModelProperty(Enum = new[] {"available", "pending", "sold"})]
+        public String Status { get; set; }
+    }
+}
